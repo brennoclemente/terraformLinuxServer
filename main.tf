8b424a84789ba12a583ba3cbf36a06e8610fd13a
brennoclemente <brennoclemente@icloud.com>
2021-03-16 17:21:23 -0300

    Create main.tf
    Nesta parte, setamos o provedor de serviço, a região a ser usada, bem como um exemplo de como subir um servidor de forma automatizada.

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# This general syntax for terraform
+# resource "<PROVIDER>_<TYPE>" "<NAME>" {
+# [CONFIG …]
+# }
+
+resource "aws_instance" "ServerA" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
+
