af9411796a1ad9f82e6c113c4bb1881d188fdcd4
brennoclemente <brennoclemente@icloud.com>
2021-03-16 18:51:15 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 resource "aws_instance" "ServerA" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.GrupodeSeguranca.id]
   
   user_data = << - EOF 
              #! / bin / bash 
