e96155576a746fe2f882bdf8180af3def4f322bb
brennoclemente <brennoclemente@icloud.com>
2021-03-16 19:30:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,29 +7,4 @@ provider "aws" {
 # [CONFIG …]
 # }
 
-resource "aws_instance" "ServerA" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.GrupodeSeguranca.id]
-  
 
-      user_data = <<-EOF
-                  #!/bin/bash
-                  echo "Hello, World" > index.html
-                  nohup busybox httpd -fp 8080 &
-                  EOF
-  
-    tags = { 
-    Name = "2ndTestedeCriacao" 
-  }
-}
-
-resource "aws_security_group" "GrupodeSeguranca" {
-  name = "Grupo-de-seguranca-ex"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
