b98f319b4b9ac1494d1cffada64e8dce81eb9af4
brennoclemente <brennoclemente@icloud.com>
2021-03-16 19:07:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "ServerA" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.GrupodeSeguranca.id]
   
-  user_data = <<-EOF 
+  user_data = <<EOF 
              #! / bin / bash 
              echo "Olá, Mundo"> index.html 
              nohup busybox httpd -f -p 8080 & EOF
