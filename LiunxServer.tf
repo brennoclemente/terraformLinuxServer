43156aaa4dff8116100d963628e8bd5f6a546512
brennoclemente <brennoclemente@icloud.com>
2021-03-17 13:57:14 -0300

    Update LiunxServer.tf

--- LiunxServer.tf
+++ LiunxServer.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ServerA" {
-  ami           = var.tipo_servidor
+  ami           = [var.tipo_servidor]
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.GrupodeSeguranca.id]
   
