de5487714cc32ab350fcf64b78aa5905ad99ae90
brennoclemente <brennoclemente@icloud.com>
2021-03-17 14:51:04 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "${var.chavedeacesso}"
+  access_key = "${var.chavedeacesso}" 
   secret_key = "${var.chavesecreta}"
 }
 
+#Já tivemos publicado aqui uma chave de acesso e secret key, porém com a necessidade de deixar o repositório publico, estas foram descomissionadas.
 # This general syntax for terraform
 # resource "<PROVIDER>_<TYPE>" "<NAME>" {
 # [CONFIG …]
