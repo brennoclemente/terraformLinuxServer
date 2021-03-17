ac5ed5b90d2b6d295901fdb3444d84771fed7186
brennoclemente <brennoclemente@icloud.com>
2021-03-17 13:58:35 -0300

    atualização sobre como tratar os tipos de servidores

--- LiunxServer.tf
+++ LiunxServer.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ServerA" {
-  ami           = [var.tipo_servidor]
+  ami           = ami-0c55b159cbfafe1f0
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.GrupodeSeguranca.id]
   
--- variables.tf
+++ variables.tf
@@ -3,20 +3,6 @@ variable "server_port" {
   type        = number
 }
 
-variable "W_Server" {
-	default = ["ami-061f91e66b9d14761"]
-}
-
-variable "L_Server" {
-	default = ["ami-0c55b159cbfafe1f0"]
-}
-
-variable "tipo_servidor" {
-	description = "Escolher o tipo de servidor a subir: Linux ou Windows"
-	type = list(string)
-	default = ["var.W_Server", "var.L_Server"]
-}
-
 variable "mensagem" {
 	description = "Digitar a mensagem a aparecer no servidor Web"
 	type = string
