4cd0adaf857fb9d12c6b0f75e635d2866aa87196
brennoclemente <brennoclemente@icloud.com>
2021-03-17 13:55:13 -0300

    Alteração campo de var.

--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@ variable "server_port" {
 }
 
 variable "W_Server" {
-	default = ami-061f91e66b9d14761
+	default = ["ami-061f91e66b9d14761"]
 }
 
 variable "L_Server" {
-	default = ami-0c55b159cbfafe1f0
+	default = ["ami-0c55b159cbfafe1f0"]
 }
 
 variable "tipo_servidor" {
