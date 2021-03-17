eb4c3a310038214f8926ce194cff3b917911110b
brennoclemente <brennoclemente@icloud.com>
2021-03-17 13:54:11 -0300

    Alteracao de nome

--- variables.tf
+++ variables.tf
@@ -3,18 +3,18 @@ variable "server_port" {
   type        = number
 }
 
-variable "WindowsServer" {
+variable "W_Server" {
 	default = ami-061f91e66b9d14761
 }
 
-variable "LinuxServer" {
+variable "L_Server" {
 	default = ami-0c55b159cbfafe1f0
 }
 
 variable "tipo_servidor" {
 	description = "Escolher o tipo de servidor a subir: Linux ou Windows"
 	type = list(string)
-	default = [var.WindowsServer, var.LinuxServer]
+	default = [var.W_Server, var.L_Server]
 }
 
 variable "mensagem" {
